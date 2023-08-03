a90db7cfbd93b77f30d06ab935310aa924ba8884
Leandro Varela Pereira <leandro.varela.ext@ipiranga.ipiranga>
2023-08-03 14:31:14 -0300

    Init Project - Docker test 0.1

---
+++ app/main.py
@@ -0,0 +1,33 @@
+#BIBLIOTECAS
+
+#FASTAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
+from routers.routers import  router
+#LOCAIS
+from config.database import criar_db
+#OUTRAS
+
+
+
+app = FastAPI()
+
+#We define authorizations for middleware components
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5432"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+#fac3c3735aea ---  docker run -d -p 5433:5433 -e POSTGRES_PORT=5433 --name docker-post postgres:latest
+
+@app.get('/')
+def get_on():
+    return {"Application On"}
+
+# criar banco de dados
+criar_db()
+
+# rotas 
+app.include_router(router)
